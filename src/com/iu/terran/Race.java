c7e838e26a7fb483bcc82c8f3011c12380658bf6
kimdaeki <kimdaeki@gmail.com>
2022-07-15 14:02:47 +0900

    add

---
+++ src/com/iu/terran/Race.java
@@ -0,0 +1,9 @@
+package com.iu.terran;
+
+import com.iu.unit.AirUnit;
+
+public class Race extends AirUnit {
+	//비행기
+	
+
+}
---
+++ src/com/iu/zerg/Hydra.java
@@ -0,0 +1,8 @@
+package com.iu.zerg;
+
+public class Hydra {
+	
+	//hp, color, name
+	//움직이고, 공격
+
+}
